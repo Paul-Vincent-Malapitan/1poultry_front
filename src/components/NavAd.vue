08b80ec66be10b81814a97263280a0a3a3307a44
paulvinzent <manalopv@gmail.com>
2023-11-24 11:21:48 +0800

    admin_ui

---
+++ src/components/NavAd.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary">
+      <v-toolbar-title>Admin</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text @click="navigateTo('user')">User Management</v-btn>
+      <v-btn text @click="navigateTo('branch')">Branch Management</v-btn>
+      <v-btn text @click="navigateTo('inventory')">Inventory</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <!-- Content will be displayed here based on the selected page -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateTo(page) {
+      this.$router.push({ name: page });
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
---
+++ src/views/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+    <NavAd />
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  // Components
+  import NavAd from '../components/NavAd.vue';
+  
+  export default defineComponent({
+    name: 'HomeView',
+    components: {
+      NavAd,
+    },
+  });
+  </script>
+  
