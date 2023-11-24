b15309f917a41b34e10bac6c34824f4c85961a04
paulvinzent <manalopv@gmail.com>
2023-11-24 12:04:21 +0800

    managements

--- src/views/BranchManagement.vue
+++ src/views/BranchManagement.vue
@@ -1,7 +1,123 @@
 <template>
-    <h1>
-        branch
-    </h1>
-</template>
-<script>
-</script>
+    <v-container>
+      <!-- Add and Manage Branches -->
+      <v-card>
+        <v-card-title>Add and Manage Branches</v-card-title>
+        <v-card-text>
+          <v-form @submit.prevent="addBranch">
+            <v-text-field v-model="newBranch.name" label="Branch Name"></v-text-field>
+            <v-text-field v-model="newBranch.address" label="Address"></v-text-field>
+            <v-text-field v-model="newBranch.contactInfo" label="Contact Information"></v-text-field>
+            <v-btn type="submit" color="primary">Add Branch</v-btn>
+          </v-form>
+  
+          <!-- Display Existing Branches -->
+          <v-list>
+            <v-list-item-group v-if="branches.length > 0">
+              <v-list-item v-for="branch in branches" :key="branch.id">
+                <v-list-item-content>
+                  <v-list-item-title>{{ branch.name }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ branch.address }}</v-list-item-subtitle>
+                  <v-list-item-subtitle>{{ branch.contactInfo }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn @click="editBranch(branch)" icon>
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                  <v-btn @click="deleteBranch(branch.id)" icon color="error">
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list-item-group>
+            <v-list-item v-else>
+              <v-list-item-content>No branches available</v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+      </v-card>
+  
+      <!-- Monitor Branch-Specific Data -->
+      <v-card>
+        <v-card-title>Monitor Branch-Specific Data</v-card-title>
+        <v-card-text>
+          <!-- You can use a chart library here to display charts and graphs -->
+          <!-- For example, using Vue Chart.js -->
+          <line-chart :data="chartData" :options="chartOptions"></line-chart>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script>
+  // Import necessary components or chart library here
+  
+  export default {
+    data() {
+      return {
+        newBranch: {
+          name: '',
+          address: '',
+          contactInfo: '',
+        },
+        branches: [
+          // List of existing branches, each branch should have an id, name, address, contactInfo, etc.
+          { id: 1, name: 'Branch 1', address: 'Address 1', contactInfo: 'Contact 1' },
+          { id: 2, name: 'Branch 2', address: 'Address 2', contactInfo: 'Contact 2' },
+          // Add more branches as needed
+        ],
+        chartData: {
+          // Chart data for monitoring branch-specific data
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: 'Sales',
+              backgroundColor: 'rgba(75,192,192,0.2)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderWidth: 1,
+              data: [65, 59, 80, 81, 56, 55, 40],
+            },
+            // Add more datasets for other data as needed
+          ],
+        },
+        chartOptions: {
+          // Chart options
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      };
+    },
+    methods: {
+      addBranch() {
+        // Add logic to add a new branch
+        console.log('Add Branch:', this.newBranch);
+        // Reset form after adding branch
+        this.newBranch = {
+          name: '',
+          address: '',
+          contactInfo: '',
+        };
+      },
+      editBranch(branch) {
+        // Add logic to edit an existing branch
+        console.log('Edit Branch:', branch);
+        // You can pre-fill the form with branch details for editing
+        this.newBranch = { ...branch };
+      },
+      deleteBranch(branchId) {
+        // Add logic to delete an existing branch
+        console.log('Delete Branch:', branchId);
+        // Remove the branch from the branches array
+        this.branches = this.branches.filter((branch) => branch.id !== branchId);
+      },
+    },
+  };
+  </script>
+  
--- src/views/UserManagement.vue
+++ src/views/UserManagement.vue
@@ -1,5 +1,125 @@
 <template>
-    <h1>user</h1>
-</template>
-<script>
-</script>
+    <v-container>
+      <!-- Create User Accounts -->
+      <v-card>
+        <v-card-title>Create User Account</v-card-title>
+        <v-card-text>
+          <v-form @submit.prevent="createUser">
+            <v-text-field v-model="newUser.name" label="Name"></v-text-field>
+            <v-text-field v-model="newUser.email" label="Email"></v-text-field>
+            <v-text-field v-model="newUser.password" label="Password" type="password"></v-text-field>
+            <v-btn type="submit" color="primary">Create User</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+  
+      <!-- Update User Accounts -->
+      <v-card>
+        <v-card-title>Update User Account</v-card-title>
+        <v-card-text>
+          <v-select v-model="selectedUserId" :items="userList" label="Select User"></v-select>
+          <v-form @submit.prevent="updateUser">
+            <v-text-field v-model="selectedUser.name" label="Name"></v-text-field>
+            <v-text-field v-model="selectedUser.email" label="Email"></v-text-field>
+            <v-text-field v-model="selectedUser.password" label="Password" type="password"></v-text-field>
+            <v-btn type="submit" color="primary">Update User</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+  
+      <!-- Delete User Accounts -->
+      <v-card>
+        <v-card-title>Delete User Account</v-card-title>
+        <v-card-text>
+          <v-select v-model="selectedUsersToDelete" :items="userList" label="Select Users to Delete" multiple></v-select>
+          <v-btn @click="deleteUsers" color="error">Delete Users</v-btn>
+        </v-card-text>
+      </v-card>
+  
+      <!-- Assign Roles and Access Permissions -->
+      <v-card>
+        <v-card-title>Assign Roles and Access Permissions</v-card-title>
+        <v-card-text>
+          <v-select v-model="selectedStaffMember" :items="staffList" label="Select Staff Member"></v-select>
+          <v-checkbox v-model="selectedRoles" :items="rolesList" label="Assign Roles"></v-checkbox>
+          <v-checkbox v-model="selectedPermissions" :items="permissionsList" label="Assign Permissions"></v-checkbox>
+          <v-btn @click="assignRolesAndPermissions" color="primary">Assign Roles and Permissions</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newUser: {
+          name: '',
+          email: '',
+          password: '',
+        },
+        userList: [
+          // List of existing users, each user should have an id, name, email, password, etc.
+          { id: 1, name: 'John Doe', email: 'john@example.com', password: 'password' },
+          { id: 2, name: 'Jane Doe', email: 'jane@example.com', password: 'password' },
+          // Add more users as needed
+        ],
+        selectedUserId: null,
+        selectedUser: {
+          name: '',
+          email: '',
+          password: '',
+        },
+        selectedUsersToDelete: [],
+        staffList: [
+          // List of staff members, each staff member should have an id, name, email, password, etc.
+          { id: 1, name: 'Staff Member 1', email: 'staff1@example.com', password: 'password' },
+          { id: 2, name: 'Staff Member 2', email: 'staff2@example.com', password: 'password' },
+          // Add more staff members as needed
+        ],
+        selectedStaffMember: null,
+        rolesList: ['Admin', 'Editor', 'Viewer'], // List of available roles
+        selectedRoles: [],
+        permissionsList: ['Read', 'Write', 'Delete'], // List of available permissions
+        selectedPermissions: [],
+      };
+    },
+    methods: {
+      createUser() {
+        // Add logic to create a new user
+        console.log('Create User:', this.newUser);
+        // Reset form after creating user
+        this.newUser = {
+          name: '',
+          email: '',
+          password: '',
+        };
+      },
+      updateUser() {
+        // Add logic to update the selected user
+        console.log('Update User:', this.selectedUser);
+        // Reset form after updating user
+        this.selectedUser = {
+          name: '',
+          email: '',
+          password: '',
+        };
+      },
+      deleteUsers() {
+        // Add logic to delete selected users
+        console.log('Delete Users:', this.selectedUsersToDelete);
+        // Clear the selection after deleting users
+        this.selectedUsersToDelete = [];
+      },
+      assignRolesAndPermissions() {
+        // Add logic to assign roles and permissions to the selected staff member
+        console.log('Assign Roles and Permissions:', this.selectedStaffMember, this.selectedRoles, this.selectedPermissions);
+        // Clear the selection after assigning roles and permissions
+        this.selectedStaffMember = null;
+        this.selectedRoles = [];
+        this.selectedPermissions = [];
+      },
+    },
+  };
+  </script>
+  
